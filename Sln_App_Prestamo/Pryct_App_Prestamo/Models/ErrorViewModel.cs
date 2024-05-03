35a79952938e2d8c5c4180d79a98496a32064466
axelcv12 <i202121233@cibertec.edu.pe>
2024-05-03 15:43:46 -0500

    1er commit

---
+++ Sln_App_Prestamo/Pryct_App_Prestamo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Pryct_App_Prestamo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
